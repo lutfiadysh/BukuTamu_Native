d65799184ad2eab32540b5725702c0b609372f1c
lutfiadysh <lutfi020117@gmail.com>
2020-01-07 08:57:18 +0700

    ganti font

--- app/src/main/java/com/example/bukutamu/InputActivity.java
+++ app/src/main/java/com/example/bukutamu/InputActivity.java
@@ -1,11 +1,20 @@
 package com.example.bukutamu;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class InputActivity extends AppCompatActivity {
 
@@ -15,12 +24,65 @@ public class InputActivity extends AppCompatActivity {
         setContentView(R.layout.activity_input);
     }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public class MyCameraActivity extends Activity
+    {
+        private static final int CAMERA_REQUEST = 1888;
+        private ImageView imageView;
+        private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_input);
+            this.imageView = (ImageView)this.findViewById(R.id.imageView1);
+            ImageButton photoButton = (ImageButton) this.findViewById(R.id.Button1);
+            photoButton.setOnClickListener(new View.OnClickListener()
+            {
+                @RequiresApi(api = Build.VERSION_CODES.M)
+                @Override
+                public void onClick(View v)
+                {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                    {
+                        requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                    }
+                    else
+                    {
+                        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+        {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (requestCode == MY_CAMERA_PERMISSION_CODE)
+            {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+                else
+                {
+                    Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data)
+        {
+            if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+            {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(photo);
+            }
         }
     }
 
